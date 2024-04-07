a0b30e33f41fbac54a43e37ed1f48c22843e8b65
peterkruhoffer <peter.kruhoffer@gmail.com>
2024-04-07 15:47:17 +0200

    quick fix: now actually stops game if 21 or higher score

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,24 +2,27 @@ import { StatusBar } from "expo-status-bar";
 import { Stack } from "expo-router";
 
 export const unstable_settings = {
-    initialRouteName: "index",
+  initialRouteName: "index",
 };
 
 export default function HomeLayout() {
-    return (
-        <>
-            <StatusBar style="dark" />
-            <Stack>
-                <Stack.Screen name="index" options={
-                    {
-                        headerTitle: "home"
-                    }
-                } />
-                <Stack.Screen name="modal" options={{
-                    presentation: "modal"
-                }}
-                />
-            </Stack>
-        </>
-    );
+  return (
+    <>
+      <StatusBar style="dark" />
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerTitle: "home",
+          }}
+        />
+        <Stack.Screen
+          name="modal"
+          options={{
+            presentation: "modal",
+          }}
+        />
+      </Stack>
+    </>
+  );
 }
